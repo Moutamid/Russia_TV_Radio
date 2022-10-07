0dc23c79fd5cc30511346055d055dea83d38eede
OsamaGhafoor <osamaghafoor58@gmail.com>
2022-10-07 22:30:16 +0500

    language implemetation

--- app/src/main/java/com/moutamid/russiatvradio/BottomSheetDialog.java
+++ app/src/main/java/com/moutamid/russiatvradio/BottomSheetDialog.java
@@ -1,15 +1,23 @@
 package com.moutamid.russiatvradio;
 
+import static android.content.Context.MODE_PRIVATE;
+import static com.moutamid.russiatvradio.Settings_Activity.SHARED_PREFS;
+import static com.moutamid.russiatvradio.Settings_Activity.TEXT1_1;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
@@ -19,6 +27,115 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
     CardView fav , settings ;
     CardView help , feed , rate ;
     CardView report ;
+
+    TextView title_main;
+    TextView title_lang;
+    TextView text_fav;
+    TextView text_setting;
+    TextView text_help;
+    TextView text_term;
+    TextView text_rate;
+    TextView text_report;
+
+    Context context;
+    Resources resources;
+    private String text1_1;
+
+
+    @Override
+    public void onStart() {
+        loadData();
+        String lang = title_lang.getText().toString().trim();
+        if (lang.equals("English")){
+            context = LocaleHelper.setLocale(getContext(), "en");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_fav.setText(resources.getString(R.string.favorites));
+            text_setting.setText(resources.getString(R.string.settings));
+            text_help.setText(resources.getString(R.string.help));
+            text_term.setText(resources.getString(R.string.policy_terms));
+            text_rate.setText(resources.getString(R.string.rate_us));
+            text_report.setText(resources.getString(R.string.report));
+        }
+        if (lang.equals("French")){
+            context = LocaleHelper.setLocale(getContext(), "fr");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_fav.setText(resources.getString(R.string.favorites));
+            text_setting.setText(resources.getString(R.string.settings));
+            text_help.setText(resources.getString(R.string.help));
+            text_term.setText(resources.getString(R.string.policy_terms));
+            text_rate.setText(resources.getString(R.string.rate_us));
+            text_report.setText(resources.getString(R.string.report));
+        }
+        if (lang.equals("German")){
+            context = LocaleHelper.setLocale(getContext(), "de");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_fav.setText(resources.getString(R.string.favorites));
+            text_setting.setText(resources.getString(R.string.settings));
+            text_help.setText(resources.getString(R.string.help));
+            text_term.setText(resources.getString(R.string.policy_terms));
+            text_rate.setText(resources.getString(R.string.rate_us));
+            text_report.setText(resources.getString(R.string.report));
+        }
+        if (lang.equals("Arabic")){
+            context = LocaleHelper.setLocale(getContext(), "ar");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_fav.setText(resources.getString(R.string.favorites));
+            text_setting.setText(resources.getString(R.string.settings));
+            text_help.setText(resources.getString(R.string.help));
+            text_term.setText(resources.getString(R.string.policy_terms));
+            text_rate.setText(resources.getString(R.string.rate_us));
+            text_report.setText(resources.getString(R.string.report));
+        }
+        if (lang.equals("Turkish")){
+            context = LocaleHelper.setLocale(getContext(), "tr");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_fav.setText(resources.getString(R.string.favorites));
+            text_setting.setText(resources.getString(R.string.settings));
+            text_help.setText(resources.getString(R.string.help));
+            text_term.setText(resources.getString(R.string.policy_terms));
+            text_rate.setText(resources.getString(R.string.rate_us));
+            text_report.setText(resources.getString(R.string.report));
+        }
+        if (lang.equals("Russian")){
+            context = LocaleHelper.setLocale(getContext(), "ru");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_fav.setText(resources.getString(R.string.favorites));
+            text_setting.setText(resources.getString(R.string.settings));
+            text_help.setText(resources.getString(R.string.help));
+            text_term.setText(resources.getString(R.string.policy_terms));
+            text_rate.setText(resources.getString(R.string.rate_us));
+            text_report.setText(resources.getString(R.string.report));
+        }
+        if (lang.equals("Spanish")){
+            context = LocaleHelper.setLocale(getContext(), "es");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_fav.setText(resources.getString(R.string.favorites));
+            text_setting.setText(resources.getString(R.string.settings));
+            text_help.setText(resources.getString(R.string.help));
+            text_term.setText(resources.getString(R.string.policy_terms));
+            text_rate.setText(resources.getString(R.string.rate_us));
+            text_report.setText(resources.getString(R.string.report));
+        }
+        if (lang.equals("Urdu")){
+            context = LocaleHelper.setLocale(getContext(), "ur");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_fav.setText(resources.getString(R.string.favorites));
+            text_setting.setText(resources.getString(R.string.settings));
+            text_help.setText(resources.getString(R.string.help));
+            text_term.setText(resources.getString(R.string.policy_terms));
+            text_rate.setText(resources.getString(R.string.rate_us));
+            text_report.setText(resources.getString(R.string.report));
+        }
+        super.onStart();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable
@@ -31,6 +148,15 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
         feed = v.findViewById(R.id.card_feedback);
         rate = v.findViewById(R.id.card_rate);
         report = v.findViewById(R.id.card_report);
+
+        title_main = v.findViewById(R.id.title_main);
+        title_lang = v.findViewById(R.id.title_lang);
+        text_fav = v.findViewById(R.id.text_fav);
+        text_setting = v.findViewById(R.id.text_setting);
+        text_help = v.findViewById(R.id.text_help);
+        text_term = v.findViewById(R.id.text_term);
+        text_rate = v.findViewById(R.id.text_rate);
+        text_report = v.findViewById(R.id.text_report);
 
         fav.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,5 +206,11 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
             }
         });
         return v;
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences(SHARED_PREFS , MODE_PRIVATE);
+        text1_1 = sharedPreferences.getString(TEXT1_1 , "English");
+        title_lang.setText(text1_1);
     }
 }
--- app/src/main/java/com/moutamid/russiatvradio/MainActivity.java
+++ app/src/main/java/com/moutamid/russiatvradio/MainActivity.java
@@ -1,12 +1,19 @@
 package com.moutamid.russiatvradio;
 
+import static com.moutamid.russiatvradio.Settings_Activity.SHARED_PREFS;
+import static com.moutamid.russiatvradio.Settings_Activity.TEXT1_1;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -17,10 +24,88 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
     ImageView tv , radio;
 
+    TextView title_main;
+    TextView title_lang;
+    TextView text_channel;
+    TextView text_radio;
+
+    Context context;
+    Resources resources;
+    private String text1_1;
+
+    @Override
+    protected void onStart() {
+        loadData();
+        String lang = title_lang.getText().toString().trim();
+        if (lang.equals("English")){
+            context = LocaleHelper.setLocale(MainActivity.this, "en");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_channel.setText(resources.getString(R.string.live_tv_channels));
+            text_radio.setText(resources.getString(R.string.live_radio));
+        }
+        if (lang.equals("French")){
+            context = LocaleHelper.setLocale(MainActivity.this, "fr");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_channel.setText(resources.getString(R.string.live_tv_channels));
+            text_radio.setText(resources.getString(R.string.live_radio));
+        }
+        if (lang.equals("German")){
+            context = LocaleHelper.setLocale(MainActivity.this, "de");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_channel.setText(resources.getString(R.string.live_tv_channels));
+            text_radio.setText(resources.getString(R.string.live_radio));
+        }
+        if (lang.equals("Arabic")){
+            context = LocaleHelper.setLocale(MainActivity.this, "ar");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_channel.setText(resources.getString(R.string.live_tv_channels));
+            text_radio.setText(resources.getString(R.string.live_radio));
+        }
+        if (lang.equals("Turkish")){
+            context = LocaleHelper.setLocale(MainActivity.this, "tr");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_channel.setText(resources.getString(R.string.live_tv_channels));
+            text_radio.setText(resources.getString(R.string.live_radio));
+        }
+        if (lang.equals("Russian")){
+            context = LocaleHelper.setLocale(MainActivity.this, "ru");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_channel.setText(resources.getString(R.string.live_tv_channels));
+            text_radio.setText(resources.getString(R.string.live_radio));
+        }
+        if (lang.equals("Spanish")){
+            context = LocaleHelper.setLocale(MainActivity.this, "es");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_channel.setText(resources.getString(R.string.live_tv_channels));
+            text_radio.setText(resources.getString(R.string.live_radio));
+        }
+        if (lang.equals("Urdu")){
+            context = LocaleHelper.setLocale(MainActivity.this, "ur");
+            resources = context.getResources();
+            title_main.setText(resources.getString(R.string.russia_tv_radio));
+            text_channel.setText(resources.getString(R.string.live_tv_channels));
+            text_radio.setText(resources.getString(R.string.live_radio));
+        }
+
+        super.onStart();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text_channel = findViewById(R.id.text_channel);
+        text_radio = findViewById(R.id.text_radio);
+        title_main = findViewById(R.id.title_main);
+        title_lang = findViewById(R.id.title_lang);
 
         fab_bottom_sheet = findViewById(R.id.fab_bottom_sheet);
         fab_bottom_sheet.setOnClickListener(new View.OnClickListener() {
@@ -60,5 +145,11 @@ public class MainActivity extends AppCompatActivity {
                 Animatoo.animateSlideLeft(MainActivity.this);
             }
         });
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS , MODE_PRIVATE);
+        text1_1 = sharedPreferences.getString(TEXT1_1 , "English");
+        title_lang.setText(text1_1);
     }
 }
